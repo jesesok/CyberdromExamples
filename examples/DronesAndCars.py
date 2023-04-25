b7df945c52b5473668e94c2d963e5d2ba3b39820
Radcl <95144752+ghorik78@users.noreply.github.com>
2023-04-25 17:14:17 +0300

    Update DronesAndCars.py
    Исправлена ошибка, при которой объекты могли уехать за пределы полигона

--- examples/DronesAndCars.py
+++ examples/DronesAndCars.py
@@ -9,20 +9,20 @@ e_2 = EdubotGCS(connection_method="udpout", ip="127.0.0.1", mavlink_port=8001)
 e_3 = EdubotGCS(connection_method="udpout", ip="127.0.0.1", mavlink_port=8002)
 
 p_1 = Pioneer(method=2, pioneer_ip="127.0.0.1", pioneer_mavlink_port=8003, logger=False)
-p_2 = Pioneer(method=2, pioneer_ip="127.0.0.2", pioneer_mavlink_port=8005, logger=False)
+p_2 = Pioneer(method=2, pioneer_ip="127.0.0.1", pioneer_mavlink_port=8005, logger=False)
 
 e_1.go_to_local_point(5, 5)
 
 while True:
     if e_1.point_reached():
-        Thread(target=e_2.go_to_local_point, args=[10, 1]).start()
-        Thread(target=e_3.go_to_local_point, args=[7.3, 4]).start()
+        Thread(target=e_2.go_to_local_point, args=[3, 1]).start()
+        Thread(target=e_3.go_to_local_point, args=[2.3, 4]).start()
 
     if e_2.point_reached():
         p_1.arm()
         p_1.takeoff()
         p_1.led_custom(mode=2, timer=10, color1=[127, 12, 41], color2=[55, 171, 42])
-        p_1.go_to_local_point(4, 9, 1.5)
+        p_1.go_to_local_point(-4, -4.9, 1.5)
 
     data = p_1.get_piro_sensor_data(blocking=True)
     if data:
@@ -34,7 +34,7 @@ while True:
         p_1.land()
         p_2.arm()
         p_2.takeoff()
-        p_2.go_to_local_point(5.0, 0, 1.5)
+        p_2.go_to_local_point(4.0, 0, 1.5)
         p_2.led_control(r=200, g=124, b=12)
 
     data = p_2.get_qr_reader_data(blocking=True)
@@ -48,4 +48,3 @@ while True:
         p_2.led_control(r=189, g=34, b=11)
 
     time.sleep(0.01)
-
